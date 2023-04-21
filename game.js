07957a462b5447cbcb35d27f8ad45d35aaa2a33e
Nina <preciouscornier13@gmail.com>
2023-04-21 18:51:42 -0400

    update

--- game.js
+++ game.js
@@ -2,20 +2,40 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 var started = false;
-var currentLevel = 0;
+var level = 0;
+$(document).keypress (function() {
+    if (!started) {
+        $("h1").text("Level: " + level);
+        nextSequence();
+        started = true;
+    }
+});
 $(".btn").click(function() {
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
     animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length-1);
 });
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        console.log("wrong");
+    }
+}
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);
-    $("h1").text("Level: " + currentLevel);
     currentLevel ++;
 }
 function playSound(name) {
@@ -28,9 +48,3 @@ function animatePress(currentColor) {
         $("#" + currentColor).removeClass("pressed");
     }, 100);
 }
-$(document).keypress (function() {
-    if (!started) {
-        nextSequence();
-        started = true;
-    }
-});
