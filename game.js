e36917523e48361a75ef895c65bbd1927dd82800
Nina <preciouscornier13@gmail.com>
2023-04-23 03:43:47 -0400

    update

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var started = false;
 var level = 0;
 $(document).keypress (function() {
     if (!started) {
-        $("h1").text("Level: " + level);
+        $("#level-title").text("Level: " + level);
         nextSequence();
         started = true;
     }
@@ -31,12 +31,14 @@ function checkAnswer(currentLevel) {
     }
 }
 function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level: " + level);
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);
-    currentLevel ++;
 }
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
